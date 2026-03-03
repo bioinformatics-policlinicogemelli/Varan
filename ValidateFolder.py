cd306534ba6910f3a6c3d834a1b246c656d0302e
Michele-Kulesko <kulesko.michele@gmail.com>
2026-03-03 15:12:20 +0100

    * remove success message when validation did not succeded

--- ValidateFolder.py
+++ ValidateFolder.py
@@ -291,8 +291,6 @@ def validate_output(
                 clean_multi(varan_input[0], "CNV", "sample_id.txt")
                 clean_multi(varan_input[0], "SNV", "single_sample_vcf")
                 clean_multi(varan_input[0], "SNV", "sample_id.txt")
-
-        logger.success("The study is ready to be uploaded on cBioportal")
         return number_for_graph
 
     error_msg = "Validation Failed!"
